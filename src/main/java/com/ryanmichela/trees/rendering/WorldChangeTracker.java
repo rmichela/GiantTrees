bdab27d31cf440362a850fb4c49af6748ddf0ed0
Ryan Michela <deltahat@gmail.com>
2014-04-29 01:10:48 -0700

    Improved rendering speed using NMS api

--- src/main/java/com/ryanmichela/trees/rendering/WorldChangeTracker.java
+++ src/main/java/com/ryanmichela/trees/rendering/WorldChangeTracker.java
@@ -1,8 +1,12 @@
 package com.ryanmichela.trees.rendering;
 
+import net.minecraft.server.v1_7_R1.ChunkSection;
+import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.v1_7_R1.CraftChunk;
+import org.bukkit.craftbukkit.v1_7_R1.util.CraftMagicNumbers;
 import org.bukkit.util.Vector;
 
 import java.util.HashMap;
@@ -34,18 +38,39 @@ public class WorldChangeTracker {
     }
 
     public void applyChanges(Location refPoint) {
-        Set<WorldChangeKey> touchedChunks = new HashSet<WorldChangeKey>();
+        Set<Chunk> touchedChunks = new HashSet<Chunk>();
 
         for (WorldChange change : changes.values()) {
             Location changeLoc = refPoint.clone().add(change.location);
             Block block = changeLoc.getBlock();
-            block.setType(change.material);
-            block.setData(change.materialData, false);
-            touchedChunks.add(new WorldChangeKey(block.getChunk().getX(), -1, block.getChunk().getZ()));
+            setBlockFast(block, change.material, change.materialData);
+            touchedChunks.add(block.getChunk());
         }
 
-        for (WorldChangeKey chunkKey : touchedChunks) {
-            refPoint.getWorld().refreshChunk(chunkKey.x, chunkKey.z);
+        for (Chunk chunk : touchedChunks) {
+            recalculateLight(chunk);
+            refPoint.getWorld().refreshChunk(chunk.getX(), chunk.getZ());
         }
+    }
+
+    private void setBlockFast(Block b, Material material, byte data) {
+        Chunk c = b.getChunk();
+        int cx = b.getX() & 0xF;
+        int cy = b.getY() & 15;
+        int cz = b.getZ() & 0xF;
+
+        net.minecraft.server.v1_7_R1.Chunk nmsChunk = ((CraftChunk) c).getHandle();
+        ChunkSection[] chunkSections = nmsChunk.i();
+        ChunkSection cs = chunkSections[b.getY() >> 4];
+        if (cs == null) {
+            cs = chunkSections[b.getY() >> 4] = new ChunkSection(b.getY() >> 4 << 4, !nmsChunk.world.worldProvider.f);
+        }
+        cs.setTypeId(cx, cy, cz, CraftMagicNumbers.getBlock(material));
+        cs.setData(cx, cy, cz, data);
+    }
+
+    private void recalculateLight(Chunk c) {
+        net.minecraft.server.v1_7_R1.Chunk nmsChunk = ((CraftChunk) c).getHandle();
+        nmsChunk.initLighting();
     }
 }
